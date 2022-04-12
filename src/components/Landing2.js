7f8a1eb7d997404614086508d1c0692c7173c88a
Virej Dasani <virej@Virejs-MacBook-Pro.local>
2022-04-12 23:07:21 +0530

    add: new preview assets

--- src/components/Landing2.js
+++ src/components/Landing2.js
@@ -91,7 +91,7 @@ export default function SplitWithImage() {
             rounded={"md"}
             alt={"feature image"}
             src={
-              "https://images.unsplash.com/photo-1554200876-56c2f25224fa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+              "https://raw.githubusercontent.com/virejdasani/Dailymoji/main/assets/img/iphone-dark-dailymoji-preview.png"
             }
             objectFit={"cover"}
           />
